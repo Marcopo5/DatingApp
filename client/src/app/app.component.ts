fb96abb8e057ed13e94813cf3292b9e22a2f747b
Marcopo5 <pongi58@gmail.com>
2020-12-03 17:06:11 +0100

    Fine sezione 6

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
 
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,8 +10,6 @@ export class HomeComponent implements OnInit {
   registerMode = false;
 
   constructor() { }
-
-
 
   ngOnInit(): void {
   }
